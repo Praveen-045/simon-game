72319c83999204e9931129f4b8c6cf51cb5e74e6
Praveen-045 <nagapuripraveen506@gmail.com>
2025-01-25 20:54:38 +0530

    made changes for overall improvements using chatgpt

--- game1.js
+++ game1.js
@@ -1,59 +1,88 @@
-level= -1;
+level = -1;
 var user = [];
 var gameflow = [];
-var color = ["green","red","yellow","blue"];
+var color = ["green", "red", "yellow", "blue"];
 var started = false;
+var soundEnabled = true; // Added flag to control sound
 
-$(document).keypress(function(){
-    if(!started){
-    started=true;
-    autogame();
+document.addEventListener('keydown', startGame); 
+document.addEventListener('click', startGame);
+
+function startGame() {
+    if (!started) {
+        started = true;
+        autogame();
     }
-})
-$(".btn").click(function(){
-    if(started){
+}
+
+// Handle click events for game buttons
+$(".btn").click(function() {
+    if (started) {
         btnanimate(this);
         user.push($(this).attr("id"));
-        var len = user.length-1;
-        if(!(user[len]===gameflow[len])){
+        var len = user.length - 1;
+        if (!(user[len] === gameflow[len])) {
             gameover();
         }
-        if(len===level){
-            user=[];
-            autogame()
+        if (len === level) {
+            user = [];
+            autogame();
         }
     }
-})
-function autogame(){
-    setTimeout(function(){
+});
+
+// Auto generate game sequence
+function autogame() {
+    setTimeout(function() {
         level++;
-        $("#level").text("level - "+level);
+        $("#level").text("Level - " + level);
         var randomNumber = Math.floor(Math.random() * 4);
         var randomChosenColour = color[randomNumber];
         gameflow.push(randomChosenColour);
-       console.log(randomChosenColour);
-       $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-       },500);
+        console.log(randomChosenColour);
+        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+        if (soundEnabled) {
+            playSound(randomChosenColour);
+        }
+    }, 500);
 }
 
-function btnanimate(e){
+// Button animation
+function btnanimate(e) {
     $(e).addClass("pressed");
-    setTimeout(function(){
+    setTimeout(function() {
         $(e).removeClass("pressed");
-    },300);
-    var sound = new Audio(e.id+".mp3");
+    }, 300);
+    if (soundEnabled) {
+        playSound(e.id);
+    }
+}
+
+// Play the sound associated with a color
+function playSound(name) {
+    var sound = new Audio(name + ".mp3");
     sound.play();
 }
-function gameover(){
+
+// Handle game over
+function gameover() {
     started = false;
-    $("#level").text("Press any key to restart the game");
-    gameflow=[];
+    $("#level").text("Game Over! Press any key to restart.");
+    gameflow = [];
     level = -1;
-    user=[];
+    user = [];
     $("body").addClass("game-over");
-    sound = new Audio("gameover.mp3");
-    sound.play();
-    setTimeout(function(){
+    if (soundEnabled) {
+        var sound = new Audio("gameover.mp3");
+        sound.play();
+    }
+    setTimeout(function() {
         $("body").removeClass("game-over");
-    },500);
+    }, 500);
 }
+
+// Mute or unmute sounds
+$("#mute").click(function() {
+    soundEnabled = !soundEnabled;
+    $(this).text(soundEnabled ? "Mute" : "Unmute");
+});
